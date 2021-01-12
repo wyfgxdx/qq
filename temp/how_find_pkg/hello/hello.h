2fb5b88bea1685bf75a40cb00680af390cdc43dd
wyfgxdx <wyfagd@163.com>
2021-01-12 06:53:24 -0800

    second commit

---
+++ temp/how_find_pkg/hello/hello.h
@@ -0,0 +1,4 @@
+#ifndef LIBhello_H_
+#define LIBhello_H_
+void helloSlam();
+#endif
---
+++ temp/how_find_pkg/usehello/main.cpp
@@ -0,0 +1,9 @@
+#include "hello.h"
+using namespace std;
+
+
+int main(int argc, char** argv)
+{
+  helloSlam();
+  return 0;
+}
